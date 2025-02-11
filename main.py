4b7063b1906e28c7b5bcf2870712ef106e4401e2
Bruno Fagundes <brunofs8@gmail.com>
2025-02-11 11:12:56 -0300

    Moved turn off feature to it own function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+def turn_off():
+    print(f"The machine is turning off..")
+    exit(0)
+
 # App Loop
 machine_on = True
-
 while machine_on:
     user_input = input("What would you like? (espresso/latte/cappuccino) ")
 
     # Detected turn off trigger
     if user_input == "off":
-        machine_on = False
-        print(f"The machine is turning off..")
-        exit(0)
+        turn_off()
