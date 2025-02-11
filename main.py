6a78b37e2f3d60e9f9eef5cf206eaaf1ec25b613
Bruno Fagundes <brunofs8@gmail.com>
2025-02-11 11:22:53 -0300

    Ammending previous commit. Added report feature.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from data import resources, MENU
+
+money_balance = 0
+
 def turn_off():
     print(f"The machine is turning off..")
     exit(0)
+
+def print_report():
+    print(f"Coffee Machine Resource Report:")
+    print(f"- Water: {resources["water"]}ml")
+    print(f"- Milk: {resources["milk"]}ml")
+    print(f"- Coffee: {resources["coffee"]}g")
+    print(f"- Money: ${money_balance}")
 
 # App Loop
 machine_on = True
@@ -10,3 +21,7 @@ while machine_on:
     # Detected turn off trigger
     if user_input == "off":
         turn_off()
+    
+    # Detected report trigger
+    elif user_input == "report":
+        print_report() 
