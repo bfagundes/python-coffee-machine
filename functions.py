fe79b77b03a36afbf5e122b185f64c6df15a807f
Bruno Fagundes <brunofs8@gmail.com>
2025-02-12 09:54:05 -0300

    refactoring and organizing code

---
+++ functions.py
@@ -0,0 +1,61 @@
+from data import resources, MENU
+money_balance = 0
+
+def turn_off():
+    print(f"The machine is turning off..")
+    exit(0)
+
+def print_report():
+    print(f"Coffee Machine Report:")
+    print(f"- Water: {resources["water"]}ml")
+    print(f"- Milk: {resources["milk"]}ml")
+    print(f"- Coffee: {resources["coffee"]}g")
+    print(f"- Money: ${money_balance:.2f}")
+
+def receive_coins():
+    quarters = int(input("How many quarters? "))
+    dimes = int(input("How many dimes? "))
+    nickels = int(input("How many nickels? "))
+    pennies = int(input("How many pennies? "))
+    total_coins = quarters*0.25 + dimes*0.1 + nickels*0.05 + pennies*0.01
+    return total_coins
+
+def enough_ingredients(drink):
+    for ingredient, amount in MENU[drink]["ingredients"].items():
+        if resources[ingredient] < amount:
+            return False
+        return True
+    
+def consume_ingredients(drink):
+    for ingredient, amount in MENU[drink]["ingredients"].items():
+        resources[ingredient] -= amount
+
+def prepare_coffee(drink):
+    if enough_ingredients:
+        consume_ingredients(drink)
+        print(f"Here is your {drink}. Enjoy!")
+
+    else:
+        print(f"Sorry we're out of {ingredient}. Please select another drink.")
+        print("Money refunded..")
+        return
+
+def receive_order(drink):
+    # Dealing with the payment
+    drink_cost = MENU[drink]["cost"]
+    print(f"The {drink} costs ${drink_cost:.2f}. Please insert coins.")
+    user_payment = receive_coins()
+    
+    if user_payment < drink_cost:
+        print("Sorry that's not enough money.")
+        print("Money refunded..")
+        return
+    
+    elif user_payment > drink_cost:
+        user_refund = user_payment - drink_cost
+        print(f"Here is ${user_refund:.2f} dollars in change.")
+
+    global money_balance
+    money_balance += drink_cost
+
+    prepare_coffee(drink)
