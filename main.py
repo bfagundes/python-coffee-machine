5a1f6659dcee712af354975e2c69666094b48449
Bruno Fagundes <brunofs8@gmail.com>
2025-02-13 13:43:49 -0300

    Refactored everything for OOP

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from functions import turn_off, print_report, receive_order
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-# App Loop
-machine_on = True
-while machine_on:
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while coffee_machine.is_powered_on:
     user_input = input("\nWhat would you like? (espresso/latte/cappuccino) ").lower()
 
     # Detected turn off trigger
     if user_input == "off":
-        turn_off()
-    
+        coffee_machine.power_off()
+        exit(0)
+
     # Detected report trigger
     elif user_input == "report":
-        print_report() 
+        coffee_machine.report()
+        money_machine.report()
 
     # Detected a valid drink order
     elif user_input in ("espresso", "latte", "cappuccino"):
-        receive_order(user_input)
+        drink = menu.find_drink(user_input)
+
+        payment_accepted = money_machine.make_payment(drink.cost)
+        
+        if payment_accepted:
+            coffee_machine.make_coffee(drink)
 
     # Option not recognized
     else:
